df93f277fd84edaae21d57f530d629d7d21b75f9
Mio Yasutake <mio.fujita.01@gmail.com>
2022-11-13 06:10:11 +0900

    initial commit

---
+++ webapp/controller/Main.controller.ts
@@ -0,0 +1,41 @@
+import MessageBox from "sap/m/MessageBox";
+import BaseController from "./BaseController";
+import formatter from "../model/formatter";
+import FileUploader from "sap/ui/unified/FileUploader";
+import FileUploaderParameter from "sap/ui/unified/FileUploaderParameter";
+import Event from "sap/ui/base/Event";
+import MessageToast from "sap/m/MessageToast";
+
+
+/**
+ * @namespace miyasuta.file2googledrive.controller
+ */
+export default class Main extends BaseController {
+	private formatter = formatter;
+
+	public async handleUploadPress(): Promise<void> {
+		const oFileUploader = this.byId("fileUploader") as FileUploader;
+		await oFileUploader.checkFileReadable();
+
+		//set headers
+		oFileUploader.removeAllHeaderParameters();
+		const fileName = oFileUploader.getValue();
+		oFileUploader.addHeaderParameter(new FileUploaderParameter({
+			name: "filename",
+			value: fileName
+		}));
+
+		oFileUploader.upload();
+	}
+
+	public handleUploadComplete(oEvent: Event): void {
+		const iHttpStatusCode = oEvent.getParameter("status") as number
+		let	sMessage: string;
+
+		if (iHttpStatusCode) {
+			sMessage = iHttpStatusCode === 200 ? "Upload Success" : "Upload Error";
+			MessageToast.show(sMessage);
+		}
+	}
+
+}
